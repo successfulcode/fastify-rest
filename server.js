b584e9d8fbf95f75c09f3bc790ad4f137671fb54
Sergej Mickevic <Sergej@successfulcode.lan>
2024-01-14 17:21:32 +0200

    Add .env file and update dependencies

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-// create a loop example  
-// 1. create a loop that will iterate 10 times
-// 2. create a loop that will iterate and print the numbers 1 through 10    
 
-const log = (message) => {
-  console.log(message);
+import Fastify from 'fastify'
+
+function buildServer() {
+
+  const server = Fastify({
+    logger: true
+  })
+
+  server.get("/", async function () {
+    return { status: "Rest api" };
+  });
+
+  server.get("/first", async function () {
+    return { status: "OK" };
+  });
+
+  return server
 }
 
-for (let i = 1; i <= 10; i++) {
-  log(i);
-}
+export default buildServer
