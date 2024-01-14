5d517ec75e4ef02413c3ea78f76fcc7252d7a528
Sergej Mickevic <Sergej@successfulcode.lan>
2024-01-14 16:03:40 +0200

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+// create a loop example  
+// 1. create a loop that will iterate 10 times
+// 2. create a loop that will iterate and print the numbers 1 through 10    
+
+const log = (message) => {
+  console.log(message);
+}
+
+for (let i = 1; i <= 10; i++) {
+  log(i);
+}
